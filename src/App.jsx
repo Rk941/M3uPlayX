280bbd4a2a0e7cc955f5495521120e3b4749b4fb
Rk941 <beastontop999@gmail.com>
2025-04-10 16:54:38 +0530

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to M3uPlayX</h1>
+      <p>Your IPTV Player is ready!</p>
+    </div>
+  );
+}
+
+export default App;
